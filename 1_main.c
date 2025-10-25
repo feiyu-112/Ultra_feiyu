4cc04fe8b0d2e44a5e64de1fd0a9f3dbde993aa1
feiyu-112 <feiyu11_2025@qq.com>
2025-10-25 13:28:51 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,4 +1,19 @@
-//
+#include <stdio.h>
+
+int main()
+{
+    float celsius,fahrenheit;
+
+    printf("请输入摄氏温度:");
+
+    scanf("%f",&celsius);
+
+    fahrenheit = celsius * 9.0 / 5.0 + 32.0;
+
+    printf("转换后的华氏温度为：%.1f\n", fahrenheit);
+
+    return 0;
+}//
 //
 //
 #include <stdio.h>
